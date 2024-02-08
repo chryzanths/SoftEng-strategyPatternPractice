8805b1aad5a6aae385166b6cac3f87b6242697cc
chryzle <104879763+chryzanths@users.noreply.github.com>
2024-02-08 08:23:02 +0800

    Update OrderDemo.java

--- OrderDemo.java
+++ OrderDemo.java
@@ -1,6 +1,6 @@
 public class OrderDemo{
     public static void main (String[] args){
-        Order order = new Order(1200.00, "Gcash");
+        Order order = new Order(1200.00, new GCash());
 
         System.out.println(order.calculatePayment());
     }
