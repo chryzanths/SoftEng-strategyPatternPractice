f3d0b25e0e606f9c40e60d8b1acea1aaa69261bf
chryzle <104879763+chryzanths@users.noreply.github.com>
2024-02-08 08:22:44 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,31 +1,14 @@
 public class Order{
     private Double orderAmount;
-    private String paymentType;
+    private PaymentType paymentType;
 
-    public Order (Double orderAmount, String paymentType){
+    public Order(Double orderAmount, PaymentType paymentType){
         this.orderAmount = orderAmount;
         this.paymentType = paymentType;
-    
+
     }
 
-    // three types of payment: gcash, maya, cash
-
     public String calculatePayment(){
-        String output = new String();
-        if (paymentType.equals("Gcash")){
-           output += "Payment is Gcash\n";
-           output += "Energy for trees: 10 points";
-
-        }
-        else if (paymentType.equals("Maya")){
-            output += "Payment is Maya\n";
-            output += "You will receive additional 1% interest on savings";
-        }
-        else {
-            output += "Payment is Cash\n";
-            output += "No incentives";
-        }
-
-        return "Your order amount is " + orderAmount + "\n" + output;
+        return "Yout order amount is " + orderAmount + paymentType.calculatePayment();
     }
 }
